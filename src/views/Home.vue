ebc877d3075a0faa442a71ec52242ce5294d17ea
Jose Gabriel Companioni Benitez <53531665+elC0mpa@users.noreply.github.com>
2021-08-14 12:44:35 -0400

    Pokemon filters component hidden waiting for fixes in graphQL API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="main-section">
     <h1 class="main-header">PokeVue</h1>
-    <pokemon-filters @type-filter="typesFilterUpdated"></pokemon-filters>
+    <!-- <pokemon-filters @type-filter="typesFilterUpdated"></pokemon-filters> -->
     <pokemons-card-wrapper :type-filters="typeFilters"></pokemons-card-wrapper>
   </main>
 </template>
@@ -10,12 +10,12 @@
 // @ is an alias to /src
 import { reactive, toRefs } from "@vue/reactivity";
 import PokemonsCardWrapper from "../components/PokemonsCardWrapper.vue";
-import PokemonFilters from "../components/PokemonFilters.vue";
+// import PokemonFilters from "../components/PokemonFilters.vue";
 export default {
   name: "Home",
   components: {
     PokemonsCardWrapper,
-    PokemonFilters,
+    // PokemonFilters,
   },
   setup() {
     const state = reactive({
