7b627f194c4f228472c9377988384aa7deda5e05
Jose Gabriel Companioni Benitez <53531665+elC0mpa@users.noreply.github.com>
2021-08-14 20:06:02 -0400

    Cretaed background of PokemonDetails view

--- src/views/PokemonDetails.vue
+++ src/views/PokemonDetails.vue
@@ -1,11 +1,41 @@
 <template>
-  <div>Pokemon Details View</div>
+  <div class="pokemon-details-section">
+    <div
+      v-if="pokemon.types"
+      class="pokemon-details-section__icon-wrapper"
+      :class="`is-${pokemon.types[0].name.toLowerCase()}`"
+    >
+      <img
+        class="pokemon-details-section__icon-wrapper--icon"
+        :src="
+          require(`../assets/icons/${pokemon.types[0].name.toLowerCase()}.svg`)
+        "
+      />
+    </div>
+    <div
+      v-if="pokemon.types"
+      class="pokemon-details-section__icon-wrapper"
+      :class="`is-${pokemon.types[
+        pokemon.types.length - 1
+      ].name.toLowerCase()}`"
+    >
+      <img
+        class="pokemon-details-section__icon-wrapper--icon"
+        :src="
+          require(`../assets/icons/${pokemon.types[
+            pokemon.types.length - 1
+          ].name.toLowerCase()}.svg`)
+        "
+      />
+    </div>
+  </div>
 </template>
 
 <script>
 import { reactive, toRefs } from "@vue/runtime-core";
 import { useRoute } from "vue-router";
-import { getPokemonDetails } from "../composables/api";
+import { useQuery } from "villus";
+import { getPokemonDetailsQuery } from "../composables/graphql-api";
 export default {
   name: "PokemonDetails",
   setup() {
@@ -13,15 +43,28 @@ export default {
       pokemon: {},
     });
     const route = useRoute();
-    getPokemonDetails(route.params.id)
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+
+    // Fecth pokemon details feature!!!!!!!!!!!!!
+    const { execute: getPokemonDetails, isFetching } = useQuery({
+      query: getPokemonDetailsQuery,
+      variables: {
+        pokemonId: parseInt(route.params.id),
+      },
+    });
+
+    getPokemonDetails().then((data) => {
+      if (data.error) {
+        console.log("Error: ", data.error);
+        return;
+      }
+      state.pokemon = data.data.pokemon;
+      console.log(state.pokemon);
+    });
+
+    //
     return {
       ...toRefs(state),
+      isFetching,
     };
   },
 };
